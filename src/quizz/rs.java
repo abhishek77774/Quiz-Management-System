ea97c8a59464de6012a04879e5b981c7e72f9b67
Abhishek Sharma <34831073+abhishek77774@users.noreply.github.com>
2018-04-21 21:13:23 +0530

    Add files via upload

---
+++ src/quizz/rs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizz;
+
+/**
+ *
+ * @author AAKASH
+ */
+class rs {
+
+    static boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getString(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
